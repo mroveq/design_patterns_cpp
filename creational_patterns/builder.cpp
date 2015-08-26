fc54a3cce10cc6103cd6a61b929c2b8d6eec7ba3
mroveq <lucas@mroveq.com>
2015-08-26 23:22:17 +0200

    Add builder design pattern
    Signed-off-by: mroveq <lucas@mroveq.com>

---
+++ creational_patterns/builder.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Flower {
+public:
+	void set_color(const string &color) { m_color = color; }
+	void set_leaf(const string &leaf) { m_leaf = leaf; }
+	void open() { std::cout << "(Flower color): " << m_color <<
+		" (leaf shape): " << m_leaf << std::endl; }
+private:
+	string m_color;
+	string m_leaf;
+};
+
+class FlowerBuilder {
+public:
+	virtual ~FlowerBuilder() {};
+	Flower* getFlower() { return m_flower; }
+	void createNewFlower() { m_flower = new Flower; }
+
+	virtual void build_color() = 0;
+	virtual void build_leaf() = 0;
+protected:
+	Flower *m_flower;
+};
+
+class flow_1 : public FlowerBuilder
+{
+public:
+	virtual ~flow_1() {};
+	virtual void build_color() { m_flower->set_color("white"); }
+	virtual void build_leaf() { m_flower->set_leaf("acicular"); }
+};
+
+class Gardener
+{
+public:
+	void setFlowerBuilder (FlowerBuilder *fb) { m_flowerBuilder = fb; }
+	Flower* getFlower() { return m_flowerBuilder->getFlower(); }
+	void constructFlower() {
+		m_flowerBuilder->createNewFlower();
+		m_flowerBuilder->build_color();
+		m_flowerBuilder->build_leaf();
+	}
+private:
+	FlowerBuilder* m_flowerBuilder;
+};
+
+int main()
+{
+	Gardener grd;
+
+	FlowerBuilder *fl_1 = new flow_1;
+	grd.setFlowerBuilder(fl_1);
+	grd.constructFlower();
+	grd.getFlower()->open();
+
+	delete fl_1;
+}
